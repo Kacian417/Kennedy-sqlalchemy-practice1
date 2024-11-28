854b517b28e5c78fd47445c990dd3314c3a505e0
Kacian417 <knicho4492@gmail.com>
2024-11-28 01:42:52 +0000

    sql alchemy practice

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,21 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(80), unique=False, nullable=False)
+    product_bar_code = db.Column(db.Integer, unique=True, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    category = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Product {self.product_name} {self.category}>'
+    
+    def serialize(self):
+        return {
+            "product_name": self.product_name,
+            "bar code number": self.product_bar_code,
+            "price": self.price,
+            "category": self.category
+        }
